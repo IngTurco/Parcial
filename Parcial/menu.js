72452d32f75105f378a7099648f35cb07a110a30
Jose Olivera <167598525+IngTurco@users.noreply.github.com>
2024-09-11 23:34:06 -0500

    Update menu.js

--- Parcial/menu.js
+++ Parcial/menu.js
@@ -1,7 +1,7 @@
-const hamburger = document.getElementById('hamburger');
-
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
 });
